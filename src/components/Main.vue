408b887ea455d90a9576d5edfb0c1a99fa37c664
steveunixson <unixson@gmail.com>
2018-03-06 20:28:10 +0300

    init commit

---
+++ src/components/Main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="Main">
+    <sale-script></sale-script>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'main',
+  components: {
+    'sale-script': () => import('./main/sale-script.vue')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/main/sale-script.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sale-script">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sale-script'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
