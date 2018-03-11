04be32194a94bd3a2c45bae240ef8d1c3df88430
Unknown <monohrom98@gmail.com>
2018-03-11 14:12:20 +0300

    add side-bar & user-slider

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="Main">
+    <div class="sidebar">
+        <user-slider></user-slider>
+    </div>
     <sale-script></sale-script>
   </div>
 </template>
@@ -8,11 +11,22 @@
 export default {
   name: 'main',
   components: {
-    'sale-script': () => import('./main/sale-script.vue')
+    'sale-script': () => import('./main/sale-script.vue'),
+    'user-slider': () => import('./main/user-slider.vue')
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .Main {
+    width: 100%;
+    height: 100%;
+  }
 
+  .sidebar {
+    width: 33vw;
+    height: 100%;
+    background-color: #605b9c;
+    float: left;
+  }
 </style>
---
+++ src/components/main/user-slider.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="user-slider">
+    <div class="slider__wrapper">
+      <div class="slider__arrow"></div>
+      <div class="slider__name">
+        <p>Vladimir Holodkov</p>
+      </div>
+      <div class="slider__arrow"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-slider'
+}
+</script>
+
+<style lang="scss" scoped>
+.slider__wrapper {
+  width: 80%;
+  height: 80px;
+  margin: 50px auto;
+  background-color: darkgrey;
+
+  .slider__name {
+    display: flex;
+    width: calc(100% - 80px);
+    height: 100%;
+    overflow-x: scroll;
+    p {
+      margin: auto;
+    }
+  }
+}
+</style>
